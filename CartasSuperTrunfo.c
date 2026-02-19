9a02daf6fda71161b197854e69da59f50d4b4533
m3ireles8 <guilherme.meireles@gmail.com>
2026-02-19 17:50:08 -0300

    Update city data handling and add calculations
    Calculo da densidade populacional e PIB per capita e exibe os resultados 
    populacao / area = densidade populacional
    pib * 1000000000 / populacao = PIB per capita (convertendo o PIB de bilhoes para reais)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,36 +6,40 @@ int main() {
 
     int numero_carta;
     char letra_carta;
-    char cidade[50];
+    char cidade [99];
     int populacao;
 
 // Solicita ao usuario os dados da carta
 
 printf("Digite o numero da carta: \n");
-scanf("%d", &numero_carta);
+scanf("%d",&numero_carta);
 
 printf("Digite a letra da carta: \n");
-scanf(" %c", &letra_carta);
+scanf(" %c",&letra_carta);
 
 printf("Digite o nome da cidade: \n");
-scanf(" %s", &cidade);
+scanf("%s",&cidade);
 
 printf("Digite a populacao: \n");
-scanf("%d", &populacao);
+scanf("%d",&populacao);
 
 printf("Digite a area da cidade: \n");
 float area;
-scanf("%f", &area);
+scanf("%f",&area);
 
 printf("Digite o PIB da cidade: \n");
 float pib;
-scanf("%f", &pib);
+scanf("%f",&pib);
 
 printf("Digite o numero de pontos turisticos: \n");
 int pontos_turisticos;
-scanf("%d", &pontos_turisticos);
+scanf("%d",&pontos_turisticos);
+
 
 // Exibe os dados da carta
+// Caluculo da densidade populacional e PIB per capita e exibe os resultados 
+// populacao / area = densidade populacional
+// pib * 1000000000 / populacao = PIB per capita (convertendo o PIB de bilhoes para reais)
 
 printf("\n------------- Dados da Carta ------------- \n");
 
@@ -44,6 +48,8 @@ printf("Populacao: %d\n", populacao);
 printf("Area: %.2f km2\n", area);
 printf("PIB: %.2f bilhoes de reais\n", pib);
 printf("Pontos Turisticos: %d\n", pontos_turisticos);
+printf("Densidade Populacional: %.2f habitantes por km2\n", populacao / area);
+printf("PIB per capita: %.2f reais\n", pib * 1000000000 / populacao);
 
     return 0;
 
